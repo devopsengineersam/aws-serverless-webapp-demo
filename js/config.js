671d91383ffc2bd9633e90282d46b7e42c41bb98
netengineersam <netengineersam.gmail.com>
2021-04-26 14:14:13 +0300

    configs

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_KzPQKaYEA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3927kj03dlhqs05s94moh7hmr9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
